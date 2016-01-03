9715b496db50cb268887992f86eaeaac288dab80
Alex Cruz <mazdarider23@gmail.com>
2016-01-03 04:07:47 +0000

    Lockscreen Clock Widget Fonts [1/2]
    This is @beanstown106's idea, we got talking while messing around with some layouts
    and decided that this should be a mod and so here it is :-)
    
    Currently there are 12 fonts to include default font, will add more as time goes by
    
    http://i.imgur.com/adKTfsM.png
    
    http://i.imgur.com/XyaACLw.jpg
    
    Change-Id: I3dccd912166643645b2b97017a7a267649ab6538
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>
    
    Conflicts:
    	packages/Keyguard/src/com/android/keyguard/KeyguardStatusView.java
    
    Conflicts:
    	packages/Keyguard/src/com/android/keyguard/KeyguardStatusView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4343,6 +4343,13 @@ public final class Settings {
         public static final String DND_WHEN_CALL = "dnd_when_call";
 
         /**
+         * Change fonts for the system lockscreen clock widget
+         *
+         * @hide
+         */
+        public static final String LOCK_CLOCK_FONTS = "lock_clock_fonts";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
