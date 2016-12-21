dcb499c3211ae7eff9970355f7fa4b425b40fe81
Pranav Vashi <neobuddy89@gmail.com>
2016-12-21 18:49:24 +0530

    QS: Add live display tile
    * This is rewrite of old live display tile by CM for 7.1
    * Revert when dynamic tiles are ready by CM.
    
    Change-Id: I941e6787021aeda3e5285a11738bfce120879a98
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -57,6 +57,7 @@ import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
+import com.android.systemui.qs.tiles.LiveDisplayTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.MusicTile;
@@ -466,6 +467,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("reboot")) return new RebootTile(this);
         else if (tileSpec.equals("music")) return new MusicTile(this);
         else if (tileSpec.equals("expanded_desktop")) return new ExpandedDesktopTile(this);
+        else if (tileSpec.equals("live_display")) return new LiveDisplayTile(this);
 
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
